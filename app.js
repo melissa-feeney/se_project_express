ad80f5c267e9556d11c9c028374bef13c04d0b70
MELISSA FEENEY <MFEENEY04@GMAIL.COM>
2025-07-24 17:32:48 -0600

    Initial commit: Express backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -14,8 +15,15 @@ mongoose
   .catch(console.error);
 
 app.use(cors());
-
 app.use(express.json());
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "http://localhost:3001",
+    changeOrigin: true,
+  })
+);
 
 app.use("/", mainRouter);
 
